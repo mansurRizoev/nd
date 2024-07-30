07651dacdc67dbd7d28ecfd20090834f3f023c54
Mansur Rizoev <mansur.rizoev.m@gmail.com>
2024-07-30 21:14:34 +0500

    wwe

---
+++ test.js
@@ -0,0 +1,28 @@
+// let name;
+// name = "Tom"
+// console.log(name);
+// name = 1234;
+// // + - / *  %
+// console.log(name + 10);
+// console.log(name - 10);
+// console.log(name * 10);
+// console.log(name / 10);
+// console.log(name % 10);
+
+/**
+ *  >
+ * <
+ * <=
+ * >=
+ * ==
+ * ===
+ * !=
+ * !==
+ * 
+ */
+"        "
+console.log("".length)
+let someData = " ";  // 0,undefined,"",'',null
+someData ||= "hello world"
+
+console.log(someData)
