827a92064127c07081b66f2b7fd82beef38bf573
Mansur Rizoev <mansur.rizoev.m@gmail.com>
2024-07-30 21:56:17 +0500

    push push

--- main.js
+++ main.js
@@ -7,8 +7,20 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello, World!</h1>');
 })
 app.get("/about", (req, res) => {
+    // console.log('Request', req);
+    console.log('Response', res);
     res.send('<h1>About Page</h1>');
 })
+app.get("/contact", (req, res) => {
+    console.log('API111', req.query);
+    res.send(`<h1>Contact Page ${req.query.abc} ${req.query.name} </h1>`);
+})
+app.get("/contact/:phone", (req, res) => {
+    console.log('Request', req.params);
+
+    res.send(`<h1>Contact Page ${req.params.phone}</h1>`);
+})
+
 app.get("/api/category", (req, res) => {
     res.json({
         message: "This is the API endpoint for categories.",
